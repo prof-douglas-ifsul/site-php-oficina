f1eaef22917d02f97334a85e3490b202206c2b95
Douglas Kellermann <douglaskellermann@ifsul.edu.br>
2022-04-25 00:18:07 -0300

    2021-12-21 (1) versão inicial - arquivos de formulário simples para veículos - arquivo hello.php para exemplos simples de instruções PHP - https://www.youtube.com/watch?v=hAFWmbbgDdM

---
+++ act_veiculos.php
@@ -0,0 +1,13 @@
+<?php
+
+$placa = $_REQUEST['placa'];
+$fabricante = $_REQUEST['fabricante'];
+$modelo = $_REQUEST['modelo'];
+$descricao = $_REQUEST['descricao'];
+
+echo "Placa: $placa<BR/>";
+echo "Fabricante: $fabricante<BR/>";
+echo "Modelo: $modelo<BR/>";
+echo "Descricao: $descricao<BR/>";
+
+?>
